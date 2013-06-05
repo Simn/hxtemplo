1130936072abfddecece7f307b2881e828330496
Simon Krajewski <simon@haxe.org>
2013-06-05 10:10:07 +0200

    basic unit tests

--- test/Test.hx
+++ test/Test.hx
@@ -1,27 +1,66 @@
-class Test {
+class Test extends haxe.unit.TestCase {
+	
+	static var whitespaceEreg = ~/[\t\n\r]*/g;
 	
 	static function main() {
-		new templo.Template(new haxe.io.StringInput(haxe.Resource.getString("macros.mtt")), "macros.mtt");
-		var tpl = new templo.Template(new haxe.io.StringInput(haxe.Resource.getString("class.mtt")), "class.mtt");
-		var str = tpl.execute([
-			"title" => "My docs",
-			"config" => {
-				title: "My Class"
-			},
-			"build" => {
-				comment: "I'm the greatest"
-			},
-			"superClassHtml" => "super class!",
-			"webmeta" => {
-				keywords: ["foo", "bar"]
-			},
-			"methods" => [ {
-				{
-					name: "myFunction"
-				}
-			}]
-		]);
-		trace(str);
+		var r = new haxe.unit.TestRunner();
+		r.add(new Test());
+		r.run();
 	}
 	
+	function testParse() {
+		var s = '<html><body onload="Hello World">Hello World</body></html>';
+		weq(s, mkt(s, [""=>""]));
+	}
+	
+	function testReplace() {
+		var s = '<html><body onload="::myValue::">::myValue::</body></html>';
+		weq('<html><body onload="Hello Var">Hello Var</body></html>', mkt(s, ["myValue" => "Hello Var"]));
+	}
+	
+	function testIf() {
+		var s = 'abc::if myVar == 3::def::end::ghi';
+		weq('abcghi', mkt(s, ["myVar" => 2]));
+		weq('abcdefghi', mkt(s, ["myVar" => 3]));
+	}
+	
+	function testElse() {
+		var s = 'abc::if myVar == 3::def::else::ghi::end::jkl';
+		weq('abcghijkl', mkt(s, ["myVar" => 2]));
+		weq('abcdefjkl', mkt(s, ["myVar" => 3]));		
+	}
+	
+	function testElseIf() {
+		var s = 'abc::if myVar == 3::def::elseif myVar == 2::ghi::else::jkl::end::mno';
+		weq('abcjklmno', mkt(s, ["myVar" => 1]));
+		weq('abcghimno', mkt(s, ["myVar" => 2]));
+		weq('abcdefmno', mkt(s, ["myVar" => 3]));		
+	}
+	
+	function testCond() {
+		var s = '<node1><node2 ::cond myVar == 3::>v</node2></node1>';
+		weq('<node1></node1>',mkt(s, ["myVar" => 1]));
+		weq('<node1><node2>v</node2></node1>',mkt(s, ["myVar" => 3]));
+	}
+	
+	function testForeach() {
+		var s = 'abc::foreach n myIterable::value::n::::end::def';
+		weq('abcdef', mkt(s, ["myIterable" => []]));
+		weq('abcvalue1def', mkt(s, ["myIterable" => [1]]));
+		weq('abcvalue1value2value3def', mkt(s, ["myIterable" => [1, 2, 3]]));
+	}
+	
+	function testRepeat() {
+		var s = '<node1><node2 ::repeat n myIterable::>value::n::</node2></node1>';
+		weq('<node1></node1>', mkt(s, ["myIterable" => []]));
+		weq('<node1><node2>value1</node2></node1>', mkt(s, ["myIterable" => [1]]));
+	}
+	
+	function mkt(s:String, map:Map<String, Dynamic>) {
+		return new templo.Template(new haxe.io.StringInput(s)).execute(map);
+	}
+	
+	function weq(expected:String, actual:String, ?p) {
+		assertEquals(whitespaceEreg.replace(expected, ""), whitespaceEreg.replace(actual, ""), p);
+	}
 }
